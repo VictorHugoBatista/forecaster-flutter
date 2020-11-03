caa901e105eb136455d516e977fa44ee9291527d
Victor Hugo Batista <victor.batista@i9xp.com.br>
2020-11-03 16:00:50 -0300

    Add the search and forecast pages main scaffolds

---
+++ lib/pages/ForecastToday.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ForecastToday extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Forecaster - today'),
+      ),
+      body: Text('today'),
+    );
+  }
+}
---
+++ lib/pages/Search.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Search extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Forecaster - Search city'),
+      ),
+      body: Text('search'),
+    );
+  }
+}
