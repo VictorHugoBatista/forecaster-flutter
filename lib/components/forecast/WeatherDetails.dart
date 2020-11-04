ca9ef2534e6686d64c9a2eac8cf0d5915fe49bfa
Victor Hugo Batista <victor.batista@i9xp.com.br>
2020-11-04 10:46:21 -0300

    Add the WeatherDetailsCard widget main structure

--- lib/components/forecast/WeatherDetails.dart
+++ lib/components/forecast/WeatherDetails.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 
+import './WeatherDetailsCard.dart';
+
 class WeatherDetails extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('details'),
+    return Row(
+      children: [
+        WeatherDetailsCard(),
+        WeatherDetailsCard(),
+      ],
     );
   }
 }
---
+++ lib/components/forecast/WeatherDetailsCard.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class WeatherDetailsCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Card(
+          child: Column(
+            children: [
+              Text('details'),
+            ]
+          ),
+        ),
+      ],
+    );
+  }
+}
