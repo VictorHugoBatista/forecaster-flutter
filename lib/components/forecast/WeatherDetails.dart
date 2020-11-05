8f55a3e4ca3b8605b34ed90e0e894d50b886cf87
Victor Hugo Batista <victor.batista@i9xp.com.br>
2020-11-05 11:27:53 -0300

    Add temperature column to weather details widget

--- lib/components/forecast/WeatherDetails.dart
+++ lib/components/forecast/WeatherDetails.dart
@@ -9,7 +9,25 @@ class WeatherDetails extends StatelessWidget {
       children: [
         WeatherDetailsCard(
           children: [
-            Text('Weather Data'),
+            Center(
+              child: Column(
+                children: [
+                  Text(
+                    'Temperature',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                  Text(
+                    '18º',
+                    style: TextStyle(
+                      fontSize: 50,
+                      fontWeight: FontWeight.w900,
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
         WeatherDetailsCard(
